12577f38e6fb876939b40b290582c418b9cd1e94
Dan G <Dangee@users.noreply.github.com>
2019-06-16 14:08:27 -0700

    Develop (#1)
    * Setup
    
    * First sets

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dgouin\box sync\development\python\github.com\dangee\tdd_experiment\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
